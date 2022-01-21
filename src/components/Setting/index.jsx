245f393d3d8d24a33e7d4565afc81fe4ed0f6475
Agnese Spinella <agnesespinella@gmail.com>
2022-01-21 18:48:02 +0100

    feat: all select done

--- src/components/Setting/index.jsx
+++ src/components/Setting/index.jsx
@@ -7,6 +7,10 @@ const Settings = () => {
     const [loading, setLoading] = useState(false);
     const [options, setOptions] = useState("")
     const [questionCategory, setQuestionCategory] = useState("")
+    const [questionDifficulty, setQuestionDifficulty] = useState("")
+    const [questionType, setQuestionType] = useState("");
+	const [numberOfQuestions, setNumberOfQuestions] = useState(10);
+
 
     useEffect(() => {
         fetch(apiUrl)
@@ -21,12 +25,24 @@ const Settings = () => {
         setQuestionCategory(e.target.value)
     }
 
+   const handleDifficultyChange = (e) => {
+        setQuestionDifficulty(e.target.value)
+    }
 
+    const handleTypeChange = (e) => {
+        setQuestionType(e.target.value)
+    }
+
+    const handleNumberChange = (e) => {
+        setNumberOfQuestions(e.target.value)
+    }
+
+ 
     if (!loading) {
         return (
+        <div>
             <div>
-                <div>
-                    <h2>Select Category:</h2>
+                    <h2>Select category:</h2>
                     <select value={questionCategory} onChange={handleCategoryChange}>
                         <option>All</option>
                         {options &&
@@ -36,8 +52,30 @@ const Settings = () => {
                                 </option>
                             ))}
                     </select>
-                </div>
             </div>
+            <div>
+                    <h2>Select difficulty:</h2>       
+                    <select value={questionDifficulty} onChange={handleDifficultyChange}>
+                        <option value="" key="diff-0">All</option>
+                        <option value="easy" key="diff-1">Easy</option>
+                        <option value="medium" key="diff-2">Medium</option>
+                        <option value="hard" key="diff-3">Hard</option>
+                    </select>        
+            </div>
+            <div>
+                    <h2>Question type:</h2>       
+                    <select value={questionType} onChange={handleTypeChange}>
+                        <option value="" key="type-0"> All </option>
+                        <option value="multiple" key="type-1"> Multiple choice</option>
+                        <option value="boolean" key="type-2"> True or False</option>
+                    </select>        
+            </div>
+            <div>
+                    <h2>Amount of questions:</h2>       
+                        <input value={numberOfQuestions} onChange={handleNumberChange}
+                        type="number" min="1"></input>
+            </div>
+        </div>
         );
     } else {
         <p>
